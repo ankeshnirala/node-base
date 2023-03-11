b6e2b74c763546513cd0ddd23fe503cd809420d8
Ankesh <ankesh@Ankeshs-MacBook-Air.local>
2023-03-12 00:34:01 +0530

    docker & docker-compose setup done

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,9 @@ import express, { json } from "express";
 import { NotFoundError } from "./errors";
 import { errorHandler } from "./middlewares";
 
-import { addCampaignRouter } from "./routers";
-import { listCampaignRouter } from "./routers";
-import { viewCampaignRouter } from "./routers";
+// import { addCampaignRouter } from "./routers";
+// import { listCampaignRouter } from "./routers";
+// import { viewCampaignRouter } from "./routers";
 
 const baseURL = "/api/v1";
 const app = express();
@@ -19,9 +19,9 @@ declare module "express-serve-static-core" {
 app.set("trust proxy", true);
 app.use(json());
 
-app.use(baseURL, addCampaignRouter);
-app.use(baseURL, listCampaignRouter);
-app.use(baseURL, viewCampaignRouter);
+// app.use(baseURL, addCampaignRouter);
+// app.use(baseURL, listCampaignRouter);
+// app.use(baseURL, viewCampaignRouter);
 
 app.all("*", () => {
   throw new NotFoundError();
